91f038f9e6b0fd769f2f1d22bea9b5eda0191e51
emichel <emicheldev@gmail.com>
2020-04-23 17:17:22 +0200

    Add validator

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
@@ -59,9 +60,16 @@ class PhoneController extends AbstractController
     /**
      * @Route("/phones", name="add_phone", methods={"POST"})
      */
-    public function new(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager)
+    public function new(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator)
     {
         $phone = $serializer->deserialize($request->getContent(), Phone::class, 'json');
+        $errors = $validator->validate($phone);
+        if(count($errors)) {
+            $errors = $serializer->serialize($errors, 'json');
+            return new Response($errors, 500, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
         $entityManager->persist($phone);
         $entityManager->flush();
         $data = [
--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 
 /**
@@ -22,18 +24,25 @@ class Phone
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"list", "show"})
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     * @Assert\Length(min="2", minMessage="Ce champ doit contenir un minimum de {{ limit }} caractères", max="255", maxMessage="Ce champ doit contenir un maximum de {{ limit }} caractères")
+
      */
     private $name;
 
     /**
      * @ORM\Column(type="text")
      * @Groups({"show"})
+     * 
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"list", "show"})
+     * @Assert\NotBlank(message="Ce champ ne doit pas être vide")
+     * @Assert\Range(min="0", minMessage="La valeur minimum autorisée est {{ limit }}", max="1500", maxMessage="La valeur maximum autorisée est {{ limit }}")
+
      */
     private $price;
 
